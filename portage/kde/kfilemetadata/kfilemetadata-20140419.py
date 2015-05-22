4a5d9d5b01aa13d6ff3b32b2b2d24a02c5522757
Christian Mollekopf <chrigi_1@fastmail.fm>
2015-05-22 17:14:27 +0200

    Fixed kfilemetadata url

--- portage/kde/kfilemetadata/kfilemetadata-20140419.py
+++ portage/kde/kfilemetadata/kfilemetadata-20140419.py
@@ -3,7 +3,7 @@ import kdedefaults as kd
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = '[git]https://github.com/kolab-groupware/kfilemetadata|kolab/integration/4.13.0|'
+        self.svnTargets['gitHEAD'] = '[git]http://git.kolab.org/diffusion/KF/kfilemetadata|kolab/integration/4.13.0|'
         for ver in ['0', '1', '2', '3', '4', '5']:
             self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
             self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver

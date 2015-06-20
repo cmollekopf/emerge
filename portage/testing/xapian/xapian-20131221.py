7694aeb98f37c79f1e0a8d3733c2fab1ad2055b9
Christian Mollekopf <chrigi_1@fastmail.fm>
2015-06-20 21:46:49 +0200

    Revert "Build a version that builds with msvc2013"
    This reverts commit a67921d7ce18db1b41f231bd48ecc930161463ae.
    
    Just yields new problems

--- portage/testing/xapian/xapian-20131221.py
+++ portage/testing/xapian/xapian-20131221.py
@@ -2,10 +2,10 @@ import info
 
 class subinfo( info.infoclass ):
     def setTargets( self ):
-        self.targets['1.2.21'] = 'http://oligarchy.co.uk/xapian/1.2.21/xapian-core-1.2.21.tar.xz'
-        self.targetInstSrc['1.2.21'] = 'xapian-core-1.2.21'
-        self.patchToApply['1.2.21'] = [('xapian-core-1.2.16-20131221.diff', 1)]
-        self.defaultTarget = '1.2.21'
+        self.targets['1.2.16'] = 'http://oligarchy.co.uk/xapian/1.2.16/xapian-core-1.2.16.tar.xz'
+        self.targetInstSrc['1.2.16'] = 'xapian-core-1.2.16'
+        self.patchToApply['1.2.16'] = [('xapian-core-1.2.16-20131221.diff', 1)]
+        self.defaultTarget = '1.2.16'
 
     def setDependencies( self ):
         self.dependencies['win32libs/zlib'] = 'default'

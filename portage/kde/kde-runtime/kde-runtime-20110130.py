a29e7048de15473346d4760213049bbb7e4efb2d
Christian Mollekopf <chrigi_1@fastmail.fm>
2015-06-29 17:13:05 +0200

    Build kde-runtime from our branch.

--- portage/kde/kde-runtime/kde-runtime-20110130.py
+++ portage/kde/kde-runtime/kde-runtime-20110130.py
@@ -3,7 +3,9 @@ import kdedefaults as kd
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = '[git]kde:%s|%s|' % (self.package, kd.kdebranch)
+        # self.svnTargets['gitHEAD'] = '[git]kde:%s|%s|' % (self.package, kd.kdebranch)
+        self.svnTargets['gitHEAD'] = '[git]https://github.com/kolab-groupware/kde-runtime.git|kolab/dev/windows|'
+        
         for ver in ['0', '1', '2', '3', '4', '5']:
             self.targets[kd.kdeversion + ver] = "http://download.kde.org/stable/" + kd.kdeversion + ver + "/src/" + self.package + "-" + kd.kdeversion + ver + ".tar.xz"
             self.targetInstSrc[kd.kdeversion + ver] = self.package + '-' + kd.kdeversion + ver

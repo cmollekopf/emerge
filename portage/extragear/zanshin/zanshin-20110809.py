7abb07e5f2e4b9396bb52166c3dea455f07346cf
Christian Mollekopf <chrigi_1@fastmail.fm>
2015-10-07 16:45:35 +0200

    Build zanshin from the regular branch again

--- portage/extragear/zanshin/zanshin-20110809.py
+++ portage/extragear/zanshin/zanshin-20110809.py
@@ -4,7 +4,7 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['gitHEAD'] = '[git]https://git.kolab.org/diffusion/Z/zanshin.git|kolab/dev/windows2|'
+        self.svnTargets['gitHEAD'] = '[git]https://git.kolab.org/diffusion/Z/zanshin.git|kolab/integration/3.0|'
         self.svnTargets['0.2-beta1'] = 'http://files.kde.org/zanshin/zanshin-0.1.81.tar.bz2'
         self.description = "a powerful yet simple application for managing your day to day actions"
         self.defaultTarget = 'gitHEAD'
